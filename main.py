3183178bf96504ac2fb00d1399535eb412c70153
Aviad Rozenhek <aviadr1@gmail.com>
2023-05-09 21:16:11 +0300

    add flask

--- main.py
+++ main.py
@@ -31,3 +31,12 @@ print(user.id, user.email, type(user))
 print('done!')
 
 
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+   return 'Hello World'
+
+if __name__ == '__main__':
+   app.run()
